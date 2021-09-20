10336e6f2d35c240d73d1225aed9caa3cfc04e74
Jhonatan Nuss <contato@jhonatanuss.dev>
2021-09-20 11:16:33 -0300

    Conectando salvando contas no banco

---
+++ src/main/java/com/jhondev/io/repository/ContasRepository.java
@@ -0,0 +1,11 @@
+package com.jhondev.io.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jhondev.io.model.Contas;
+
+@Repository
+public interface ContasRepository extends CrudRepository<Contas, Long> {
+
+}
