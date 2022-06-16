2c0b49daa1872e8923fdc093d131b8643b8fadf4
jhonatan.nuss <contato@jhonatanuss.dev>
2022-06-16 11:33:29 -0300

    Ajustes API

--- src/main/java/com/jhondev/io/controller/UsuarioController.java
+++ src/main/java/com/jhondev/io/controller/UsuarioController.java
@@ -25,56 +25,40 @@ import com.jhondev.io.repository.UsuarioRepository;
 
 @CrossOrigin
 @RestController /* Arquitetura REST */
-@RequestMapping(value = "/usuario")
+@RequestMapping(value = "/api/finance")
 public class UsuarioController {
 
-	@Autowired /* Se fosse CDI seria @ Inject */
+	@Autowired
 	private UsuarioRepository usuarioRepository;
 
-	/* Serviço RESTful */
-	@GetMapping(value = "/{id}/codigovenda/{venda}", produces = "application/json")
-	public ResponseEntity<Usuario> relatorio(@PathVariable(value = "id") Long id 
-			                                ,@PathVariable(value = "venda") Long venda) {
 
-		Optional<Usuario> usuario = usuarioRepository.findById(id);
+	@PostMapping(value = "usuario/cadastrar", produces = "application/json")
+	public ResponseEntity<Usuario> cadastrar(@RequestBody Usuario usuario) {
 
-		return new ResponseEntity<Usuario>(usuario.get(), HttpStatus.OK);
+		String encriptPassword = new BCryptPasswordEncoder().encode(usuario.getSenha());
+		usuario.setSenha(encriptPassword);
+		Usuario usuarioSalvo = usuarioRepository.save(usuario);
+
+		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
+
 	}
 
-	/* LISTAR todos os usuários */
-	@GetMapping(value = "/", produces = "application/json")
-	@CacheEvict(value = "cacheusuarios", allEntries = true)
-	@CachePut("cacheusuarios")
-	public ResponseEntity<List<Usuario>> usuario() throws InterruptedException {
+	@GetMapping(value = "/usuarios", produces = "application/json")
+	public ResponseEntity<List<Usuario>> listarUsuarios() throws InterruptedException {
 
 		List<Usuario> list = (List<Usuario>) usuarioRepository.findAll();
-		
+
 		return new ResponseEntity<List<Usuario>>(list, HttpStatus.OK);
 	}
 
-	/* LISTAR usuário pelo ID */
-	@GetMapping(value = "{id}", produces = "application/json", headers = "X-API-Version=v1")
-	public ResponseEntity<Usuario> initV1(@PathVariable(value = "id") Long id) {
+	@GetMapping(value = "usuario/{id}", produces = "application/json")
+	public ResponseEntity<Usuario> usuarioById(@PathVariable(value = "id") Long id) {
 
 		Optional<Usuario> usuario = usuarioRepository.findById(id);
-        System.out.println("Execultando versão 1");
-		return new ResponseEntity<Usuario>(usuario.get(), HttpStatus.OK);
-	}
-	
-	/* LISTAR usuário pelo ID */
-	@GetMapping(value = "{id}", produces = "application/json", headers = "X-API-Version=v2")
-	@Cacheable("cacheuser")
-	public ResponseEntity<Usuario> initV2(@PathVariable(value = "id") Long id) {
-
-		Optional<Usuario> usuario = usuarioRepository.findById(id);
-        System.out.println("Execultando versão 2");
 		return new ResponseEntity<Usuario>(usuario.get(), HttpStatus.OK);
 	}
 
-	
-
-	/* DELETAR usuário pelo ID */
-	@DeleteMapping(value = "/{id}", produces = "application/text")
+	@DeleteMapping(value = "usuario/{id}", produces = "application/text")
 	public String delete(@PathVariable(value = "id") Long id) {
 
 		usuarioRepository.deleteById(id);
@@ -82,21 +66,8 @@ public class UsuarioController {
 		return "ok";
 	}
 
-	/* CADASTRAR um usuário */
-	@PostMapping(value = "/", produces = "application/json")
-	public ResponseEntity<Usuario> cadastrar(@RequestBody Usuario usuario) {
-
-		String senhacriptografada = new BCryptPasswordEncoder().encode(usuario.getSenha());
-		usuario.setSenha(senhacriptografada);
-		Usuario usuarioSalvo = usuarioRepository.save(usuario);
-
-		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
-
-	}
-
-	/* ATUALIZAR um usuário */
-	@PutMapping(value = "/", produces = "application/json")
-	public ResponseEntity<Usuario> atualizar(@RequestBody Usuario usuario) {
+	@PutMapping(value = "usuario/atualizar", produces = "application/json")
+	public ResponseEntity<Usuario> atualizarUsuario(@RequestBody Usuario usuario) {
 		
 		Usuario userTemporario = usuarioRepository.findUserByLogin(usuario.getLogin());
 
