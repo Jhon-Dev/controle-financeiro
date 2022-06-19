de244e7ddfaa87c6b922566d34d6f238d70023a9
Jhonatan Nuss <contato@jhonatanuss.dev>
2022-06-19 12:35:16 -0300

    Criando endpoint de atualização de Lancamento

--- backend/src/main/java/com/jhondev/io/controller/LancamentoController.java
+++ backend/src/main/java/com/jhondev/io/controller/LancamentoController.java
@@ -1,11 +1,13 @@
 package com.jhondev.io.controller;
 
 import com.jhondev.io.model.Lancamento;
+import com.jhondev.io.model.Usuario;
 import com.jhondev.io.repository.LancamentoRepository;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,12 +19,12 @@ import java.util.List;
 public class LancamentoController {
 
 	@Autowired
-	private LancamentoRepository contaRepository;
+	private LancamentoRepository lancamentoRepository;
 
 	@GetMapping(value = "/lancamentos", produces = "application/json")
 	public ResponseEntity<List<Lancamento>> listaLancamentos() throws InterruptedException {
 
-		List<Lancamento> list = (List<Lancamento>) contaRepository.findAll();
+		List<Lancamento> list = (List<Lancamento>) lancamentoRepository.findAll();
 
 		return new ResponseEntity<List<Lancamento>>(list, HttpStatus.OK);
 
@@ -31,7 +33,7 @@ public class LancamentoController {
 	@PostMapping(value = "/lancamento/cadastrar", produces = "application/json")
 	public ResponseEntity<Lancamento> cadastrar(@RequestBody Lancamento lancamento) {
 
-		Lancamento lancamentoSalvo = contaRepository.save(lancamento);
+		Lancamento lancamentoSalvo = lancamentoRepository.save(lancamento);
 
 		return new ResponseEntity<Lancamento>(lancamentoSalvo, HttpStatus.OK);
 	}
@@ -39,9 +41,18 @@ public class LancamentoController {
 	/*DELETAR*/
 	@DeleteMapping(value = "/lancamento/{id}", produces = "application/text")
 	public String delete(@PathVariable(value = "id") Long id) {
-		
-		contaRepository.deleteById(id);
+
+		lancamentoRepository.deleteById(id);
 		
 		return "Lançamento deletado";
+	}
+
+	@PutMapping(value = "lancamento/atualizar", produces = "application/json")
+	public ResponseEntity<Lancamento> atualizarLancamento(@RequestBody Lancamento lancamento) {
+
+		Lancamento lancamentoAtualizado = lancamentoRepository.save(lancamento);
+
+		return new ResponseEntity<Lancamento>(lancamentoAtualizado, HttpStatus.OK);
+
 	}
 }
